7a37fe0ca1d75dcf8c03649d8f8df87b1a33b92b
Пользователь <kyy101000@gmail.com>
2021-08-31 18:47:56 +0600

    Hibernate properties are added

--- src/main/java/kg/itschool/demo/entity/Passenger.java
+++ src/main/java/kg/itschool/demo/entity/Passenger.java
@@ -3,21 +3,45 @@ package kg.itschool.demo.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
-
+@Entity
+@Table(name="tb_passengers")
 @Data
 @NoArgsConstructor
 public class Passenger {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
+
+    @Column
     private String firstName;
+
+    @Column
     private String lastName;
+
+    @Column
     private String email;
+
+    @Column
     private String userName;
+
+    @Column
     private String password;
+
+    @Column
     private String phoneNumber;
+
+    @Column
     private Date birthDate;
+
+    @Column
     private String sex;
+
+    @Column
     private String passportDetails;
+
+    @Column
     private String userRole;
 }
--- src/main/java/kg/itschool/demo/entity/Plane.java
+++ src/main/java/kg/itschool/demo/entity/Plane.java
@@ -3,16 +3,27 @@ package kg.itschool.demo.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-
+@Entity
+@Table(name = "tb_planes")
 @Data
 @NoArgsConstructor
 public class Plane {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
+
+    @Column
     @NotNull
     private String company;
+
+    @Column
     @NotNull
     private String model;
+
+    @Column
     @NotNull
     private Integer seats;
 }
