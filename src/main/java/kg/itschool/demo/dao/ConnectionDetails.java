47c5360bed294622084eb2e2101387576e2fa320
Пользователь <kyy101000@gmail.com>
2021-08-31 18:27:39 +0600

    Database connection details are changed

--- src/main/java/kg/itschool/demo/dao/ConnectionDetails.java
+++ src/main/java/kg/itschool/demo/dao/ConnectionDetails.java
@@ -7,9 +7,9 @@ import java.sql.Driver;
 
 public interface ConnectionDetails {
 
-    String PASSWORD = "4b34543515bc311e15bda7f4cf056bdf593bf046ea893f878ae4fc830245a543";
-    String USERNAME = "njmvhayozbzlox";
-    String URl = "jdbc:postgresql://ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d647ao85hj8qfs";;
+    String PASSWORD = "woshiyouli10";
+    String USERNAME = "postgres";
+    String URl = "jdbc:postgresql://localhost:5432/postgres";
 
     static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URl,USERNAME,PASSWORD);
